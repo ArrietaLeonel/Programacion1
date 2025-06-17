5e79d7454a1be91b6469399ce82a9cbe008402bc
Arrieta Leonel <arrieta.o.leonel@gmail.com>
2025-06-17 10:27:49 -0300

    Ad feature Search

--- WinForms/Program.cs
+++ WinForms/Program.cs
@@ -13,7 +13,7 @@ namespace WinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PeliculasView());
+            Application.Run(new FavGamesView());
         }
     }
 }
